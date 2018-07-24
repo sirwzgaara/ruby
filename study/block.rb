64dc6ede7ba6401e832d562a4a3cb5b6162a5608
e450c-linux <1819644289@qq.com>
2018-07-24 11:31:49 +0800

    modified:   block.rb 	new file:   moral.rb 	new file:   trig.rb

--- study/block.rb
+++ study/block.rb
@@ -1,3 +1,6 @@
+#encoding:gbk
+
+=begin
 def f1
 	yield
 end
@@ -11,6 +14,17 @@ end
 f1 { puts "f1"}
 f2 { puts "f2"}
 f3 ( proc { puts "f3"})
-f3 ( -> {puts "f3"})
 f3 ( Proc.new{puts "f3"})
+f3 ( -> {puts "f3"})
 f3 ( lambda{puts "f3"})
+=end
+
+#$LOAD_PATH << '.'
+#require 'trig.rb'
+#require 'moral'
+
+require_relative '/home/ruby/study/trig.rb'
+require_relative '/home/ruby/study/moral.rb'
+
+y = Trig.sin(Trig::PI/4)
+wrongdoing = Moral.sin(Moral::VERY_BAD)
---
+++ study/trig.rb
@@ -0,0 +1,9 @@
+module Trig
+    PI = 3.1415936
+    def Trig.sin(x)
+        puts "sinx is #{x}"
+    end
+    def Trig.cos(x)
+        puts "cosx is #{x}"
+    end
+end
